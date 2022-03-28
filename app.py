7e1938a2c8d429f19e8cf55e3d0cc27ef6d3e431
Divya Yash <87994860+Valium-stash-on-Floor13@users.noreply.github.com>
2022-03-28 04:25:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -13,15 +13,18 @@ def home_page():
     return render_template('home.html', languages=lang, sen1=sen1, sen2=sen2 )
 
 
-@app.route('/translate_lang', methods=['POST'])
-def translate_lang():
+@app.route('/translate', methods=['POST', "GET"])
+def translate():
+    lang =[{"name": "English", "code": "en"}, {"name": "Hindi", "code": "hi"},{"name": "Spanish", "code": "es"},{"name": "German", "code": "de"}, {"name": "Chinese", "code": "zh-cn"}]
     if request.method == "POST":
-        lang =[{"name": "English", "code": "en"}, {"name": "Hindi", "code": "hi"},{"name": "Spanish", "code": "es"},{"name": "German", "code": "de"}, {"name": "Chinese", "code": "zh-cn"}]
         code= str(request.form["code"])
         translator= Translator()
+        print(hi)
         s1= translator.translate("Chat With Me!", src="en", dest="code").text
         s2= translator.translate("Hi, I am Medbot and I'm here to help you!", src="en", dest="code").text
-    return render_template('home.html',sen1=s1, sen2=s2, code=code, languages=lang)
+        return redirect("/home", sen1=s1, sen2=s2, code=code, languages=lang)
+    else:
+    return render_template('home.html',sen1="Chat with me!", sen2="Hi, I am Medbot and I'm here to help you!", code="en", languages=lang)
 
 @app.route('/get')
 def get_bot_response():
