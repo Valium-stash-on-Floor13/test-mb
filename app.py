7403dc6621071c61f12cbe0215ee1aa3d0d0b7cb
Divya Yash <87994860+Valium-stash-on-Floor13@users.noreply.github.com>
2022-03-28 03:54:30 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+from googletrans import Translator
+
 app = Flask(__name__)
 from bot import medbot
 
@@ -11,6 +13,15 @@ def home_page():
     return render_template('home.html', languages=lang, sen1=sen1, sen2=sen2 )
 
 
+@app.route('/translate_lang', methods=['POST'])
+def translate_lang():
+    if request.method == "POST":
+        lang =[{"name": "English", "code": "en"}, {"name": "Hindi", "code": "hi"},{"name": "Spanish", "code": "es"},{"name": "German", "code": "de"}, {"name": "Chinese", "code": "zh-cn"}]
+        code= str(request.form["code"])
+        translator= Translator()
+        s1= translator.translate("Chat With Me!", src="en", dest="code").text
+        s2= translator.translate("Hi, I am Medbot and I'm here to help you!", src="en", dest="code").text
+    return render_template('home.html',s1=s1, s2=s2, lang_code=code, languages=lang)
 
 @app.route('/get')
 def get_bot_response():
